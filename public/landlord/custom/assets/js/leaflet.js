b899fd158d2f78abef936294cd1cde1b7eb2d68b
Mutahir Shah <mutahir@codeenterprise.com>
2026-04-01 17:44:21 +0500

    composer file modified

--- public/landlord/custom/assets/js/leaflet.js
+++ public/landlord/custom/assets/js/leaflet.js
@@ -4,8 +4,11 @@
     var leafletBasic = function () {
         if ($('#map-basic').length) {
             var mymap = L.map('map-basic').setView([51.505, -0.09], 13);
+            const mapboxToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
-                 maxZoom: 18,
+
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${mapboxToken}', {
+                maxZoom: 18,
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
                     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -48,7 +51,8 @@
         if ($('#map-choropleth').length) {
             var map = L.map('map-choropleth').setView([37.8, -96], 4);
 
-                 maxZoom: 18,
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${mapboxToken}', {
+                maxZoom: 18,
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
                     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
