85ed984e2e9ad6196aea319578b8f31e8220e131
Mutahir Shah <mutahiricup@gmail.com>
2026-05-02 17:24:35 +0500

    auto deployments

---
+++ deploy.php
@@ -0,0 +1,15 @@
+<?php
+
+$secret = "MY_SECRET_KEY";
+
+if ($_GET['key'] !== $secret) {
+    die("Access denied");
+}
+// composer install --no-dev &&
+echo shell_exec('
+cd /home/u472752505/domains/ismailfashionparadise.com/public_html/projects/posdemo &&
+git pull origin development &&
+php artisan migrate --force &&
+php artisan cache:clear &&
+php artisan config:cache
+2>&1');
